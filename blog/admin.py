d4bafd4b56a4ee2cc55cf84420c524baf81e660d
caleb jephuneh <calebjephunneh@gmail.com>
2023-04-15 21:13:20 +0300

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+# blog/admin.py
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
